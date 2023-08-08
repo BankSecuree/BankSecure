5fdc8a232c868a27b2a8b59f83af090655ea54f6
brunolimabh <bruno.lima@sptech.school>
2023-08-08 18:09:43 -0300

    cleanup: Limpando o código

--- web-data-viz-main/Site Institucional/public/js/main.js
+++ web-data-viz-main/Site Institucional/public/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Arsha
-* Updated: Jul 27 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
