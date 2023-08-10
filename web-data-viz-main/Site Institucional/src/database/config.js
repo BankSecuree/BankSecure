f7595f46de38b35b7187adfee8eb8783e867823c
brunolimabh <bruno.lima@sptech.school>
2023-08-10 13:41:01 -0300

    refactor (Login): Conexão com o banco

--- web-data-viz-main/Site Institucional/src/database/config.js
+++ web-data-viz-main/Site Institucional/src/database/config.js
@@ -19,10 +19,10 @@ var sqlServerConfig = {
 
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
-    host: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    host: "localhost",
+    database: "bankSecure",
+    user: "user_bankSecure",
+    password: "urubu100",
 };
 
 function executar(instrucao) {
