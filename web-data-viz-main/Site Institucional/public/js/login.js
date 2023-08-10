e97251990cc34074f0d406afacce0e9668910dd7
brunolimabh <bruno.lima@sptech.school>
2023-08-10 12:09:13 -0300

    refactor (Login): Linkando paginas

---
+++ web-data-viz-main/Site Institucional/public/js/login.js
@@ -0,0 +1,90 @@
+function entrar() {
+    // aguardar();
+    
+    var emailVar = yourUsername.value;
+    var senhaVar = yourPassword.value;
+    var erroEntrar = false
+
+    // if (emailVar == '' || emailVar.indexOf('@') == -1 || emailVar.indexOf('.') == -1 ) {
+    //     erroEntrar = true;
+    //     erroE1.style.display = 'block';
+    //     ipt_email.style.border = '2px solid red';
+    // } else {
+    //     erroE1.style.display = 'none';
+    //     ipt_email.style.border = '2px solid #32a7b1';
+    // }
+    // if (senhaVar == '' || senhaVar.length < 6) {
+    //     erroEntrar = true;
+    //     erroS1.style.display = 'block';
+    //     ipt_senha.style.border = '2px solid red';
+    // } else {
+    //     erroS1.style.display = 'none';
+    //     ipt_senha.style.border = '2px solid #32a7b1';
+    // }
+
+    if (erroEntrar == true) {
+        // finalizarAguardar();
+        return false;
+    }
+    else {
+        // setInterval(sumirMensagem, 5000)
+
+        console.log("FORM LOGIN: ", emailVar);
+        console.log("FORM SENHA: ", senhaVar);
+
+        fetch("/usuario/autenticar", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                emailServer: emailVar,
+                senhaServer: senhaVar
+            })
+        }).then(function (resposta) {
+            console.log("ESTOU NO THEN DO entrar()!")
+
+            if (resposta.ok) {
+                console.log(resposta);
+
+                resposta.json().then(json => {
+                    console.log(json);
+                    console.log(JSON.stringify(json));
+                    
+                    sessionStorage.ID_FUNCIONARIO = json.idFuncionario;
+                    sessionStorage.NOME_FUNCIONARIO = json.nome;
+                    sessionStorage.CARGO_FUNCIONARIO = json.cargo;
+                    sessionStorage.EMAIL_FUNCIONARIO = json.email;
+                    sessionStorage.FK_SUPERIOR = json.fkSuperior;
+                    sessionStorage.FK_FILIAIS = json.filiais;
+
+                    setTimeout(function () {
+                        window.location = "./dashboard/index.html";
+                    }, 2000); 
+
+                });
+                // cardErroLogin.style.display = "block"
+                // cardErroLogin.style.border = "2px solid greenyellow"
+                // cardErroLogin.style.color = "greenyellow"
+                // mensagem_erroLogin.innerHTML = "✅Entrando! Aguarde...✅";
+            } else {
+                // cardErroLogin.style.display = "block"
+                // cardErroLogin.style.border = "2px solid red"
+                // cardErroLogin.style.color = "red"
+                // mensagem_erroLogin.innerHTML = "❌Conta não cadastrada❌";
+            
+                console.log("Houve um erro ao tentar realizar o login!");
+
+                resposta.text().then(texto => {
+                    console.error(texto);
+                    // finalizarAguardar(texto);
+                });
+            }
+
+        }).catch(function (erro) {
+            console.log(erro);
+        })
+
+        return false;
+    }
+}
