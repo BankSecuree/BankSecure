a2c4e1fa8dd91d71d6b3120433b6dd273f2ded34
KauanCursino <kauan.souza@sptech.school>
2023-08-10 09:35:00 -0300

    feat(Banco de Dados): inclusão de tabelas de acordo com a modelagem de dados

--- web-data-viz-main/Site Institucional/src/database/script-desenvolvimento.sql
+++ web-data-viz-main/Site Institucional/src/database/script-desenvolvimento.sql
@@ -4,12 +4,47 @@ CREATE DATABASE bankSecure;
 
 USE bankSecure;
 
+
+CREATE TABLE Empresa(
+idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+nomeEmpresa VARCHAR (50),
+cnpj CHAR (14)
+);
+
+CREATE TABLE Login(
+idLogin INT PRIMARY KEY AUTO_INCREMENT,
+emailFunc VARCHAR(50),
+senhaFunc VARCHAR(50)
+);
+
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50,)
-	foto VARCHAR(255)
+    cpfFunc CHAR(14),
+    gerente INT,
+	foto VARCHAR(255),
+    fkEmpresa INT,
+    fkLogin INT,
+    FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa),
+    FOREIGN KEY (fkLogin) REFERENCES login(idLogin)
+);
+
+CREATE TABLE maquina(
+idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+MaquinaCpu INT,
+MaquinaDemoria INT,
+MaquinaDisco INT, 
+fkEmpresa INT,
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(fkEmpresa)
+);
+CREATE TABLE registros(
+dataHora DATETIME PRIMARY KEY,
+usoMemoria INT,
+usoDisco INT,
+usoCPU INT,
+tempCPU VARCHAR(10),
+fkEmpresa INT,
+FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
 CREATE USER 'user_bankSecure'@'localhost' IDENTIFIED BY 'urubu100';
