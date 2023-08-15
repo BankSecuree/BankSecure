3f2f33a5f2d4124b49eddce1c44f5f3b035f816e
brunolimabh <bruno.lima@sptech.school>
2023-08-15 13:58:12 -0300

    refactor(Usuarios): Adicionando botão de deletar usuario

--- web-data-viz-main/Site Institucional/src/models/usuariosModel.js
+++ web-data-viz-main/Site Institucional/src/models/usuariosModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function exibirTabelaUsuarios(gerente) {
     console.log("ACESSEI O USUARIOS  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function exibirTabelaUsuarios()");
     var instrucao = `
-    SELECT us.nome, em.nomeEmpresa as empresa, (SELECT COUNT(idUsuario) FROM usuario WHERE gerente = ${gerente}) as funcionarios, us.dataInicio
+    SELECT us.idUsuario, us.nome, em.nomeEmpresa as empresa, (SELECT COUNT(idUsuario) FROM usuario WHERE gerente = ${gerente}) as funcionarios, us.dataInicio
 	    FROM usuario as us, empresa as em WHERE gerente = ${gerente};
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
