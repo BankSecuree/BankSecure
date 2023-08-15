a4dd9a364ac37d996a81b65eaf39a23c25fafb7d
brunolimabh <bruno.lima@sptech.school>
2023-08-15 12:19:02 -0300

    refactor(BD): Script do delimiter

--- web-data-viz-main/Site Institucional/src/database/script-desenvolvimento.sql
+++ web-data-viz-main/Site Institucional/src/database/script-desenvolvimento.sql
@@ -6,7 +6,7 @@ USE bankSecure;
 CREATE TABLE empresa(
 idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 nomeEmpresa VARCHAR (50),
-cnpj CHAR (14)
+cnpj CHAR (18)
 );
 
 CREATE TABLE usuario (
@@ -16,6 +16,7 @@ CREATE TABLE usuario (
 	nome VARCHAR(50),
     cpf CHAR(14),
     telefone CHAR(14),
+    dataNascimento DATE,
     gerente INT,
 	foto VARCHAR(255),
     fkEmpresa INT,
@@ -40,8 +41,24 @@ fkEmpresa INT,
 FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
+select * from usuario;
+
+DELIMITER //
+CREATE PROCEDURE cadastrar_usuario(IN 
+	us_nome VARCHAR(50), us_cpf CHAR(14), us_telefone CHAR(14), us_dataNascimento DATE,
+    em_nomeEmpresa VARCHAR (50), em_cnpj CHAR(18)
+)
+BEGIN
+	INSERT INTO usuario (nome, cpf, telefone, dataNascimento) 
+		VALUES (us_nome, us_cpf, us_telefone, us_dataNascimento);
+	INSERT INTO empresa (nomeEmpresa, cnpj) 
+		VALUES (em_nomeEmpresa, em_cnpj);
+END//
+DELIMITER ;
+
 DROP USER 'user_bankSecure'@'localhost';
 CREATE USER 'user_bankSecure'@'localhost' IDENTIFIED BY 'urubu100';
 GRANT ALL ON bankSecure.* TO 'user_bankSecure'@'localhost';
+GRANT EXECUTE ON PROCEDURE cadastrar_usuario to 'user_bankSecure'@'localhost';
 FLUSH PRIVILEGES;
 
