53f53bb285bead576986872272930355c54ec857
brunolimabh <bruno.lima@sptech.school>
2023-08-24 09:31:57 -0300

    fix: Corrigindo caminho do bot SLACK

--- web-data-viz-main/bot/botSlack.py
+++ web-data-viz-main/bot/botSlack.py
@@ -1,8 +1,8 @@
 import requests 
 import json
 
-mensagem = {"text":"Ola, Paulo!"}
-chatItau = "https://hooks.slack.com/services/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"
+mensagem = {"text":"Ola, Itaú!"}
+chatItau = "https://hooks.slack.com/services/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"
 
 postMsg = requests.post(chatItau, data=json.dumps(mensagem))
-# print(postMsg.status_code)
+print(postMsg.status_code)
