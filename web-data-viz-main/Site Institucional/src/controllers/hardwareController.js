5ea14cf2fb2b445ff7a78b5460670587ba819dba
KauanCursino <kauan.souza@sptech.school>
2023-08-25 16:36:12 -0300

    feat(cadastro de máquinas): cadastro de NOME e fkAgencia funcionais

--- web-data-viz-main/Site Institucional/src/controllers/hardwareController.js
+++ web-data-viz-main/Site Institucional/src/controllers/hardwareController.js
@@ -1,25 +1,70 @@
 var hardwareModel = require("../models/hardwareModel")
 
-function cadastrarHardware(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+function cadastrarNomeMaquina(req, res){
+    var nomeMaquina = req.body.nomeMaquinaServer;
+    var fkAgencia = req.body.fkAgenciaServer;
+
+    if (nomeMaquina == undefined) {
+        res.status(400).send("O nome da sua máquina está undefined")
+    } else if(fkAgencia == undefined){
+        res.status(400).send("A sua fkAgencia está undefined")
+    }else{
+
+        hardwareModel.cadastrarNomeMaquina(nomeMaquina, fkAgencia)
+            .then(
+                function(resultado){
+                    res.json(resultado);
+                }
+            ).catch(
+                function(erro){
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o cadastro!Erro: ",
+                    erro.sqlMessage
+                    );
+                    res.status(500).send(erro.sqlMessage);
+                }
+            )
+    }
+}
+
+
+
+function cadastrarHardwareCpu(req, res) {
+    
     var cpu = req.body.cpuServer;
-    var memoria = req.body.memoriaServer;
-    var disco = req.body.discoServer;
-    var temperatura = req.body.temperaturaServer;
     
-    
-    // Faça as validações dos valores
     if (cpu == undefined) {
         res.status(400).send("Sua cpu está undefined!");
-    } else if (memoria == undefined) {
+    } else {
+        hardwareModel.cadastrarHardwareCpu(cpu)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            )
+    }
+}
+
+function cadastrarHardwareMemoria(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var memoria = req.body.memoriaServer;
+   
+    
+    // Faça as validações dos valores
+    if (memoria == undefined) {
         res.status(400).send("Sua memória está undefined!");
-    } else if (disco == undefined) {
-        res.status(400).send("Seu disco está undefined!");
-    }else if (temperatura == undefined) {
-        res.status(400).send("Seu limite de temperatura está undefined!");
     }else {
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        hardwareModel.cadastrarHardware(cpu, memoria, disco, temperatura)
+        hardwareModel.cadastrarHardwareMemoria(memoria)
             .then(
                 function (resultado) {
                     res.json(resultado);
@@ -36,6 +81,36 @@ function cadastrarHardware(req, res) {
             );
     }
 }
+
+function cadastrarHardwareDisco(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var disco = req.body.discoServer;
+   
+    
+    // Faça as validações dos valores
+    if (disco == undefined) {
+        res.status(400).send("Sua memória está undefined!");
+    }else {
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        hardwareModel.cadastrarHardwareMemoria(disco)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
-    cadastrarHardware
+    cadastrarNomeMaquina,
+    cadastrarHardwareCpu
 }
--- web-data-viz-main/Site Institucional/src/models/hardwareModel.js
+++ web-data-viz-main/Site Institucional/src/models/hardwareModel.js
@@ -1,19 +1,53 @@
 var database = require("../database/config");
 
-function cadastrarHardware(cpu, memoria, disco, temperatura){
+function cadastrarNomeMaquina(nomeMaquina, fkAgencia){
     console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarHardware()");
     var instrucao = `
-    INSERT INTO maquina (maquinaCpu, maquinaMemoria, maquinaDisco, maquinaTemperatura) VALUES(
-    "${cpu}",
-    ${memoria},
-    ${disco},
-    ${temperatura}
-    )
+    INSERT INTO maquina (nome, fkAgencia) VALUES("${nomeMaquina}",${fkAgencia})
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+
+
+function cadastrarHardwareCpu(cpu){
+    console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarHardware()");
+    var instrucao = `
+    INSERT INTO componente (nome, unidadeMedida) VALUES ("${cpu}", "GHz");
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 
 }
+
+function cadastrarHardwareMemoria(memoria){
+    console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarHardware()");
+    var instrucao = `
+    INSERT INTO componente (nome, unidadeMedida) VALUES("${memoria}", "GB)
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrarHardwareDisco(disco){
+    console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarHardware()");
+    var instrucao = `
+    INSERT INTO componente (nome, unidadeMedida) VALUES("${disco}", "KB")
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+function cadastrarHardwareTemperatura(temperatura){
+    console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarHardware()");
+    var instrucao = `
+    INSERT INTO componente (nome, unidadeMedida) VALUES("${temperatura}", "°C")
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
 module.exports = {
-    cadastrarHardware
+    cadastrarNomeMaquina,
+    cadastrarHardwareCpu
 };
