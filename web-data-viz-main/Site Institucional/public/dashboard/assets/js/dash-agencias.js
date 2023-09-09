bf5ae39e08adba1e9334f84904ade44650d1dd11
beatriz <beatriz.rocha@sptech.school>
2023-09-09 12:05:32 -0300

    feat(dash-agencias.js): gráfico exibindo dados do BD em andamento

--- web-data-viz-main/Site Institucional/public/dashboard/assets/js/dash-agencias.js
+++ web-data-viz-main/Site Institucional/public/dashboard/assets/js/dash-agencias.js
@@ -1,70 +1,80 @@
+const sql = require("sql");
+
+const conn = sql.createConnection({
+  host: "localhost",
+  database: "bankSecure",
+  user: "user_bankSecure",
+  password: "Urubu_100",
+});
+
 function exibirListaAgencias() {
-  fetch(`/dashAgencias/exibirListaAgencias/${sessionStorage.ID_USUARIO}`).then(function (resposta) {
-    if (resposta.ok) {
-      var lista = document.getElementById("listaAgencias");
-      var trColunas = document.createElement("tr");
-      var thead = document.createElement("thead");
-      var thId = document.createElement("th");
-      thId.setAttribute("scope", "row");
-      thId.innerHTML = "#";
-      var thApelido = document.createElement("th");
-      thApelido.setAttribute("scope", "row");
-      thApelido.innerHTML = "Apelido";
-      var thExibir = document.createElement("th");
-      thExibir.setAttribute("scope", "row");
-      thExibir.innerHTML = "Exibir";
-
-      trColunas.appendChild(thId);
-      trColunas.appendChild(thApelido);
-      trColunas.appendChild(thExibir);
-      thead.appendChild(trColunas);
-      lista.appendChild(thead);
-
-      if (resposta.status == 204) {
+  fetch(`/dashAgencias/exibirListaAgencias/${sessionStorage.ID_USUARIO}`)
+    .then(function (resposta) {
+      if (resposta.ok) {
         var lista = document.getElementById("listaAgencias");
-        var mensagem = document.createElement("p");
-        mensagem.innerHTML = "Nenhum resultado encontrado."
-        lista.innerHTML = "";
-        lista.appendChild(mensagem);
-        throw "Nenhum resultado encontrado!!";
-      }
-      resposta.json().then(function (resposta) {
-        var contId = 0;
-        for (let i = 0; i <= resposta.length; i++) {
-          console.log(i)
-          console.log(publicacao)
+        var trColunas = document.createElement("tr");
+        var thead = document.createElement("thead");
+        var thId = document.createElement("th");
+        thId.setAttribute("scope", "row");
+        thId.innerHTML = "#";
+        var thApelido = document.createElement("th");
+        thApelido.setAttribute("scope", "row");
+        thApelido.innerHTML = "Apelido";
+        var thExibir = document.createElement("th");
+        thExibir.setAttribute("scope", "row");
+        thExibir.innerHTML = "Exibir";
 
+        trColunas.appendChild(thId);
+        trColunas.appendChild(thApelido);
+        trColunas.appendChild(thExibir);
+        thead.appendChild(trColunas);
+        lista.appendChild(thead);
+
+        if (resposta.status == 204) {
           var lista = document.getElementById("listaAgencias");
-          var publicacao = resposta[i];
-
-          var thNumero = document.createElement("th");
-          thNumero.innerHTML = publicacao.idAgencia;
-          var tdApelido = document.createElement("td");
-          tdApelido.innerHTML = publicacao.apelido;
-          // var tdCnpj = document.createElement("td");
-          // tdCnpj.innerHTML = publicacao.cnpjAgencia;
-          var tdButton = document.createElement("td");
-          tdButton.innerHTML = `<a onclick="exibirGrafico(${publicacao.idAgencia})" class="btn btn-primary btn-sm" title="Exibir gráfico da agência"><i class="bi bi-graph-up"></i></a>`;
-          var tr = document.createElement("tr");
-          var tbody = document.createElement("tbody");
-
-          tr.appendChild(thNumero);
-          tr.appendChild(tdApelido);
-          // tr.appendChild(tdCnpj);
-          tr.appendChild(tdButton);
-          tbody.appendChild(tr);
-          lista.appendChild(tbody);
+          var mensagem = document.createElement("p");
+          mensagem.innerHTML = "Nenhum resultado encontrado.";
+          lista.innerHTML = "";
+          lista.appendChild(mensagem);
+          throw "Nenhum resultado encontrado!!";
         }
-      });
-    } else {
-      throw ('Houve um erro na API!');
-    }
-  }).catch(function (resposta) {
-    console.error(resposta);
-    // finalizarAguardar();
-  });
-}
+        resposta.json().then(function (resposta) {
+          var contId = 0;
+          for (let i = 0; i <= resposta.length; i++) {
+            console.log(i);
+            console.log(publicacao);
 
+            var lista = document.getElementById("listaAgencias");
+            var publicacao = resposta[i];
+
+            var thNumero = document.createElement("th");
+            thNumero.innerHTML = publicacao.idAgencia;
+            var tdApelido = document.createElement("td");
+            tdApelido.innerHTML = publicacao.apelido;
+            // var tdCnpj = document.createElement("td");
+            // tdCnpj.innerHTML = publicacao.cnpjAgencia;
+            var tdButton = document.createElement("td");
+            tdButton.innerHTML = `<a onclick="exibirGrafico(${publicacao.idAgencia})" class="btn btn-primary btn-sm" title="Exibir gráfico da agência"><i class="bi bi-graph-up"></i></a>`;
+            var tr = document.createElement("tr");
+            var tbody = document.createElement("tbody");
+
+            tr.appendChild(thNumero);
+            tr.appendChild(tdApelido);
+            // tr.appendChild(tdCnpj);
+            tr.appendChild(tdButton);
+            tbody.appendChild(tr);
+            lista.appendChild(tbody);
+          }
+        });
+      } else {
+        throw "Houve um erro na API!";
+      }
+    })
+    .catch(function (resposta) {
+      console.error(resposta);
+      // finalizarAguardar();
+    });
+}
 
 function graficoAgencia() {
   // var options = {
@@ -121,265 +131,293 @@ function graficoAgencia() {
 
   window.Apex = {
     chart: {
-      foreColor: 'black',
+      foreColor: "black",
       toolbar: {
-        show: false
+        show: false,
       },
     },
-    colors: ['#FCCF31', '#17ead9', '#f02fc2'],
+    colors: ["#FCCF31", "#17ead9", "#f02fc2"],
     stroke: {
-      width: 3
+      width: 3,
     },
     dataLabels: {
-      enabled: false
+      enabled: false,
     },
     grid: {
       borderColor: "#40475D",
     },
     xaxis: {
       axisTicks: {
-        color: '#333'
+        color: "#333",
       },
       axisBorder: {
-        color: "#333"
-      }
+        color: "#333",
+      },
     },
     fill: {
-      type: 'gradient',
+      type: "gradient",
       gradient: {
-        gradientToColors: ['#F55555', '#6078ea', '#6094ea']
+        gradientToColors: ["#F55555", "#6078ea", "#6094ea"],
       },
     },
     yaxis: {
       decimalsInFloat: 2,
       opposite: true,
       labels: {
-        offsetX: -10
-      }
-    }
+        offsetX: -10,
+      },
+    },
   };
 
-  function getRangeRandom(yrange) {
-    return Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+  function getDadosMaquina(yrange) {
+    const query = `
+      SELECT MAX(Cpu)
+      FROM vw_registrosEstruturados
+      WHERE dataHora = CURRENT_TIMESTAMP
+      `;
+
+    conn.query(query, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const cpu = result[0].Cpu;
+        console.log(cpu);
+      }
+    });
   }
 
   var optionsProgress1 = {
     chart: {
       height: 70,
-      type: 'bar',
+      type: "bar",
       stacked: true,
       sparkline: {
-        enabled: true
-      }
+        enabled: true,
+      },
     },
     plotOptions: {
       bar: {
         horizontal: true,
-        barHeight: '20%',
+        barHeight: "20%",
         colors: {
-          backgroundBarColors: ['#40475D']
-        }
+          backgroundBarColors: ["#40475D"],
+        },
       },
     },
     stroke: {
       width: 0,
     },
-    series: [{
-      name: 'CPU',
-      data: [44]
-    }],
+    series: [
+      {
+        name: "CPU",
+        data: [44],
+      },
+    ],
     title: {
       floating: true,
       offsetX: -10,
       offsetY: 5,
-      text: 'CPU'
+      text: "CPU",
     },
     subtitle: {
       floating: true,
-      align: 'right',
+      align: "right",
       offsetY: 0,
-      text: '44%',
+      text: "44%",
       style: {
-        fontSize: '20px'
-      }
+        fontSize: "20px",
+      },
     },
     tooltip: {
-      enabled: false
+      enabled: false,
     },
     xaxis: {
-      categories: ['CPU'],
+      categories: ["CPU"],
     },
     yaxis: {
-      max: 100
+      max: 100,
     },
     fill: {
-      opacity: 1
-    }
-  }
+      opacity: 1,
+    },
+  };
 
-  var chartProgress1 = new ApexCharts(document.querySelector('#progress1'), optionsProgress1);
+  var chartProgress1 = new ApexCharts(
+    document.querySelector("#progress1"),
+    optionsProgress1
+  );
   chartProgress1.render();
-
 
   var optionsProgress2 = {
     chart: {
       height: 70,
-      type: 'bar',
+      type: "bar",
       stacked: true,
       sparkline: {
-        enabled: true
-      }
+        enabled: true,
+      },
     },
     plotOptions: {
       bar: {
         horizontal: true,
-        barHeight: '20%',
+        barHeight: "20%",
         colors: {
-          backgroundBarColors: ['#40475D']
-        }
+          backgroundBarColors: ["#40475D"],
+        },
       },
     },
-    colors: ['#17ead9'],
+    colors: ["#17ead9"],
     stroke: {
       width: 0,
     },
-    series: [{
-      name: 'Memória',
-      data: [80]
-    }],
+    series: [
+      {
+        name: "Memória",
+        data: [80],
+      },
+    ],
     title: {
       floating: true,
       offsetX: -10,
       offsetY: 5,
-      text: 'Memória'
+      text: "Memória",
     },
     subtitle: {
       floating: true,
-      align: 'right',
+      align: "right",
       offsetY: 0,
-      text: '80%',
+      text: "80%",
       style: {
-        fontSize: '20px'
-      }
+        fontSize: "20px",
+      },
     },
     tooltip: {
-      enabled: false
+      enabled: false,
     },
     xaxis: {
-      categories: ['Memória'],
+      categories: ["Memória"],
     },
     yaxis: {
-      max: 100
+      max: 100,
     },
     fill: {
-      type: 'gradient',
+      type: "gradient",
       gradient: {
         inverseColors: false,
-        gradientToColors: ['#6078ea']
-      }
+        gradientToColors: ["#6078ea"],
+      },
     },
-  }
+  };
 
-  var chartProgress2 = new ApexCharts(document.querySelector('#progress2'), optionsProgress2);
+  var chartProgress2 = new ApexCharts(
+    document.querySelector("#progress2"),
+    optionsProgress2
+  );
   chartProgress2.render();
-
 
   var optionsProgress3 = {
     chart: {
       height: 70,
-      type: 'bar',
+      type: "bar",
       stacked: true,
       sparkline: {
-        enabled: true
-      }
+        enabled: true,
+      },
     },
     plotOptions: {
       bar: {
         horizontal: true,
-        barHeight: '20%',
+        barHeight: "20%",
         colors: {
-          backgroundBarColors: ['#40475D']
-        }
+          backgroundBarColors: ["#40475D"],
+        },
       },
     },
-    colors: ['#f02fc2'],
+    colors: ["#f02fc2"],
     stroke: {
       width: 0,
     },
-    series: [{
-      name: 'Disco',
-      data: [74]
-    }],
+    series: [
+      {
+        name: "Disco",
+        data: [74],
+      },
+    ],
     fill: {
-      type: 'gradient',
+      type: "gradient",
       gradient: {
-        gradientToColors: ['#6094ea']
-      }
+        gradientToColors: ["#6094ea"],
+      },
     },
     title: {
       floating: true,
       offsetX: -10,
       offsetY: 5,
-      text: 'Disco'
+      text: "Disco",
     },
     subtitle: {
       floating: true,
-      align: 'right',
+      align: "right",
       offsetY: 0,
-      text: '74%',
+      text: "74%",
       style: {
-        fontSize: '20px'
-      }
+        fontSize: "20px",
+      },
     },
     tooltip: {
-      enabled: false
+      enabled: false,
     },
     xaxis: {
-      categories: ['Disco'],
+      categories: ["Disco"],
     },
     yaxis: {
-      max: 100
+      max: 100,
     },
-  }
+  };
 
-  var chartProgress3 = new ApexCharts(document.querySelector('#progress3'), optionsProgress3);
+  var chartProgress3 = new ApexCharts(
+    document.querySelector("#progress3"),
+    optionsProgress3
+  );
   chartProgress3.render();
 
   window.setInterval(function () {
-
     //mexer aqui para passar as variaveis do banco com parametro
 
-    var p1Data = getRangeRandom({ min: 10, max: 100 });
+    var p1Data = getDadosMaquina({ min: 10, max: 100 });
     chartProgress1.updateOptions({
-      series: [{
-        data: [p1Data]
-      }],
+      series: [
+        {
+          data: [p1Data],
+        },
+      ],
       subtitle: {
-        text: p1Data + "%"
-      }
-    })
+        text: p1Data + "%",
+      },
+    });
 
-    var p2Data = getRangeRandom({ min: 10, max: 100 });
+    var p2Data = getDadosMaquina({ min: 10, max: 100 });
     chartProgress2.updateOptions({
-      series: [{
-        data: [p2Data]
-      }],
+      series: [
+        {
+          data: [p2Data],
+        },
+      ],
       subtitle: {
-        text: p2Data + "%"
-      }
-    })
+        text: p2Data + "%",
+      },
+    });
 
-    var p3Data = getRangeRandom({ min: 10, max: 100 });
+    var p3Data = getDadosMaquina({ min: 10, max: 100 });
     chartProgress3.updateOptions({
-      series: [{
-        data: [p3Data]
-      }],
+      series: [
+        {
+          data: [p3Data],
+        },
+      ],
       subtitle: {
-        text: p3Data + "%"
-      }
-    })
-
-
-
+        text: p3Data + "%",
+      },
+    });
   }, 3000);
 }
