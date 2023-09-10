7df00bc4b4a326132dc0404797a6a57715fb1cdf
brunolimabh <bruno.lima@sptech.school>
2023-09-10 15:34:05 -0300

    fix: Corrigindo slack e jira para a sprint 1

--- API/createIssueJira.py
+++ API/createIssueJira.py
@@ -1,10 +1,28 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+from datetime import datetime
+
+data = datetime.now()
+data = data.strftime('%Y/%m/%d %H:%M:%S')
+
+mensagem = {"text": f"""
+            🚨ALERTA🚨
+
+            Protocolo  => 837021
+            Data          => {data}
+            User          => Máquina X
+            Descrição  => {"Sua memória RAM ultrapassou:"} XX%  
+            """}
+chatItau = "https://hooks.slack.com/services/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"
+
+postMsg = requests.post(chatItau, data=json.dumps(mensagem))
+
+
 
 url = "https://banksecure.atlassian.net/rest/api/3/issue"
 
-auth = HTTPBasicAuth("suporte.banksecure@gmail.com", "ATATT3xFfGF0TY7WvTgqpW5R9DIp048nQsbeGDwgD1d1JZb2K0HTtsg_yLZBPveoCvdDTBgFyzTduecMwA6lVMU-6t7d6ARhMAkNTXRww2SbcOgKZjWiyFHpFuErB32pKdztM2E0BgHG0N0Q9QriPaoNVX8ixtPwUzouu5Sc7EvIUUPky33N4-4=5768639E")
+auth = HTTPBasicAuth("suporte.banksecure@gmail.com", "ATATT3xFfGF0h03C81f-lcuaJpPPNBlhmUIEsl0ceC9eQAEKS0mLypJmI1reJf2MX6jSsIhltcuvbOIdXFMJIoBKLHBrmuZcqouHo-6HJTlXt33ACtMs_KU2mX_7YLSpe3h1-hsI3QTUmjM2euTKjTuybLXQKDU_WYw_CdBSjvuaLUPUJFHvZTI=D00309ED")
 
 headers = {
   "Accept": "application/json",
@@ -13,8 +31,8 @@ headers = {
 
 payload = json.dumps({
     "fields":{
-        "summary": "ALERTA DO SLACK",
-        "project":{"key":"BSB"},
+        "summary": "837021 - ALERTA DO SLACK",
+        "project":{"key":"BSITAU"},
         'issuetype': {'name': 'General request'},
         "description": {"content": [{"content": [
                                       {
