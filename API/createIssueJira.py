2920b3effa83b1aadacef3538df8351d9c15335f
brunolimabh <bruno.lima@sptech.school>
2023-09-11 09:37:03 -0300

    refactor: Create issue Jira

--- API/createIssueJira.py
+++ API/createIssueJira.py
@@ -2,55 +2,63 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 from datetime import datetime
+import psutil
 
-data = datetime.now()
-data = data.strftime('%Y/%m/%d %H:%M:%S')
+repetir = True
+while repetir:
+  data = datetime.now()
+  data = data.strftime('%Y/%m/%d %H:%M:%S')
+  ram_percent = psutil.virtual_memory().percent
+  user = [user[0] for user in psutil.users()]
+  user = user[0]
 
-mensagem = {"text": f"""
-            🚨ALERTA🚨
+  if (ram_percent >= 80):
+    repetir = False
+    mensagem = {"text": f"""
+                🚨ALERTA🚨
 
-            Protocolo  => 837021
-            Data          => {data}
-            User          => Máquina X
-            Descrição  => {"Sua memória RAM ultrapassou:"} XX%  
-            """}
-chatItau = "https://hooks.slack.com/services/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"
+                Protocolo  => 837021
+                Data          => {data}
+                User          => ${user}
+                Descrição  => {"Sua memória RAM ultrapassou:"} {ram_percent}%  
+                """}
+    chatItau = "https://hooks.slack.com/services/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"
 
-postMsg = requests.post(chatItau, data=json.dumps(mensagem))
+    postMsg = requests.post(chatItau, data=json.dumps(mensagem))
 
 
 
-url = "https://banksecure.atlassian.net/rest/api/3/issue"
+    url = "https://banksecure.atlassian.net/rest/api/3/issue"
 
-auth = HTTPBasicAuth("suporte.banksecure@gmail.com", "ATATT3xFfGF0h03C81f-lcuaJpPPNBlhmUIEsl0ceC9eQAEKS0mLypJmI1reJf2MX6jSsIhltcuvbOIdXFMJIoBKLHBrmuZcqouHo-6HJTlXt33ACtMs_KU2mX_7YLSpe3h1-hsI3QTUmjM2euTKjTuybLXQKDU_WYw_CdBSjvuaLUPUJFHvZTI=D00309ED")
+    auth = HTTPBasicAuth("suporte.banksecure@gmail.com", "ATATT3xFfGF08LobAfSeXkqk1KRpSciYfZiZivdvAznaP8Xxby91xg9uNlg7VbJa6JYpWigvebjO9qvCvCjrh7eDV_Nhctmt-m9-oknCJl60EFEGS_NEn-17qzJl2L8MNvALO0a5veSRE5kN2q8mNTmfT7w3-MUsiVa9a-PmFxe9NJwswQZCpKw=EE3CC2AE")
 
-headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json"
-}
+    headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
 
-payload = json.dumps({
-    "fields":{
-        "summary": "837021 - ALERTA DO SLACK",
-        "project":{"key":"BSITAU"},
-        'issuetype': {'name': 'General request'},
-        "description": {"content": [{"content": [
-                                      {
-                                        "text": "CONFIRA SEUS ALERTAS!!!!",
-                                        "type": "text"
-                                      }],
-                                     "type": "paragraph"}],
-                                    "type": "doc",
-                                    "version": 1}
-      }
-})
+    payload = json.dumps({
+        "fields":{
+            "summary": "837021 - ALERTA DO SLACK",
+            "project":{"key":"BSITAU"},
+            'issuetype': {'name': 'General request'},
+            "description": {"content": [{"content": [
+                                          {
+                                            "text": "CONFIRA SEUS ALERTAS!!!!",
+                                            "type": "text"
+                                          }],
+                                        "type": "paragraph"}],
+                                        "type": "doc",
+                                        "version": 1}
+          }
+    })
 
-response = requests.request(
-   "POST",
-   url,
-   data=payload,
-   headers=headers,
-   auth=auth
-)
+    response = requests.request(
+      "POST",
+      url,
+      data=payload,
+      headers=headers,
+      auth=auth
+    )
 
-print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+    print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
