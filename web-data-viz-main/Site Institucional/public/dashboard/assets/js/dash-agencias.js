81e2948cda9ef6ae48ae5ff4ae2cabc98a76d950
beatriz <beatriz.rocha@sptech.school>
2023-09-11 15:30:12 -0300

    fix: exibição dados máquina

--- web-data-viz-main/Site Institucional/public/dashboard/assets/js/dash-agencias.js
+++ web-data-viz-main/Site Institucional/public/dashboard/assets/js/dash-agencias.js
@@ -134,53 +134,259 @@ function graficoAgencia(idAgencia, maquinas) {
   })})
 
   for (let i = 0; i < maquinas; i++) {
+
+    var card = document.getElementById("div");
+
+    var cardTitle = document.createElement("h5");
+    var colMd7 = document.createElement("div");
+    var boxMt4 = document.createElement("div");
+    var mt4 = document.createElement("div");
+    var progress1 = document.createElement("div");
+    var progress2 = document.createElement("div");
+    var progress3 = document.createElement("div");
+
+    cardTitle.setAttribute("class", "card-title");
+    colMd7.setAttribute("class", "col-md-7");
+    boxMt4.setAttribute("class", "box mt-4");
+    mt4.setAttribute("class", "mt-4");
+    progress1.setAttribute("class", "progress1");
+    progress2.setAttribute("class", "progress2");
+    progress3.setAttribute("class", "progress3");
+
+    colMd7.appendChild(boxMt4);
+    boxMt4.appendChild(mt4);
+    mt4.appendChild(progress1, progress2, progress3);
+    card.appendChild();
+
+
+
     console.log(maquinas)
+    
+    window.Apex = {
+      chart: {
+        foreColor: 'black',
+        toolbar: {
+          show: false
+        },
+      },
+      colors: ['#FCCF31', '#17ead9', '#f02fc2'],
+      stroke: {
+        width: 3
+      },
+      dataLabels: {
+        enabled: false
+      },
+      grid: {
+        borderColor: "#40475D",
+      },
+      xaxis: {
+        axisTicks: {
+          color: '#333'
+        },
+        axisBorder: {
+          color: "#333"
+        }
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          gradientToColors: ['#F55555', '#6078ea', '#6094ea']
+        },
+      },
+      yaxis: {
+        decimalsInFloat: 2,
+        opposite: true,
+        labels: {
+          offsetX: -10
+        }
+      }
+    };
+
+    var optionsProgress1 = {
+      chart: {
+        height: 70,
+        type: 'bar',
+        stacked: true,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+          barHeight: '20%',
+          colors: {
+            backgroundBarColors: ['#40475D']
+          }
+        },
+      },
+      stroke: {
+        width: 0,
+      },
+      series: [{
+        name: 'CPU',
+        data: [44]
+      }],
+      title: {
+        floating: true,
+        offsetX: -10,
+        offsetY: 5,
+        text: 'CPU'
+      },
+      subtitle: {
+        floating: true,
+        align: 'right',
+        offsetY: 0,
+        text: '44%',
+        style: {
+          fontSize: '20px'
+        }
+      },
+      tooltip: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['CPU'],
+      },
+      yaxis: {
+        max: 100
+      },
+      fill: {
+        opacity: 1
+      }
+    }
+  
+    var chartProgress1 = new ApexCharts(document.querySelector('.progress1'), optionsProgress1);
+    chartProgress1.render();
+  
+  
+    var optionsProgress2 = {
+      chart: {
+        height: 70,
+        type: 'bar',
+        stacked: true,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+          barHeight: '20%',
+          colors: {
+            backgroundBarColors: ['#40475D']
+          }
+        },
+      },
+      colors: ['#17ead9'],
+      stroke: {
+        width: 0,
+      },
+      series: [{
+        name: 'Memória',
+        data: [80]
+      }],
+      title: {
+        floating: true,
+        offsetX: -10,
+        offsetY: 5,
+        text: 'Memória'
+      },
+      subtitle: {
+        floating: true,
+        align: 'right',
+        offsetY: 0,
+        text: '80%',
+        style: {
+          fontSize: '20px'
+        }
+      },
+      tooltip: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['Memória'],
+      },
+      yaxis: {
+        max: 100
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          inverseColors: false,
+          gradientToColors: ['#6078ea']
+        }
+      },
+    }
+  
+    var chartProgress2 = new ApexCharts(document.querySelector('.progress2'), optionsProgress2);
+    chartProgress2.render();
+  
+  
+    var optionsProgress3 = {
+      chart: {
+        height: 70,
+        type: 'bar',
+        stacked: true,
+        sparkline: {
+          enabled: true
+        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+          barHeight: '20%',
+          colors: {
+            backgroundBarColors: ['#40475D']
+          }
+        },
+      },
+      colors: ['#f02fc2'],
+      stroke: {
+        width: 0,
+      },
+      series: [{
+        name: 'Disco',
+        data: [74]
+      }],
+      fill: {
+        type: 'gradient',
+        gradient: {
+          gradientToColors: ['#6094ea']
+        }
+      },
+      title: {
+        floating: true,
+        offsetX: -10,
+        offsetY: 5,
+        text: 'Disco'
+      },
+      subtitle: {
+        floating: true,
+        align: 'right',
+        offsetY: 0,
+        text: '74%',
+        style: {
+          fontSize: '20px'
+        }
+      },
+      tooltip: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['Disco'],
+      },
+      yaxis: {
+        max: 100
+      },
+    }
+  
+    var chartProgress3 = new ApexCharts(document.querySelector('.progress3'), optionsProgress3);
+    chartProgress3.render();
   }
 
 }
 
-
-
-
-  // window.Apex = {
-  //   chart: {
-  //     foreColor: 'black',
-  //     toolbar: {
-  //       show: false
-  //     },
-  //   },
-  //   colors: ['#FCCF31', '#17ead9', '#f02fc2'],
-  //   stroke: {
-  //     width: 3
-  //   },
-  //   dataLabels: {
-  //     enabled: false
-  //   },
-  //   grid: {
-  //     borderColor: "#40475D",
-  //   },
-  //   xaxis: {
-  //     axisTicks: {
-  //       color: '#333'
-  //     },
-  //     axisBorder: {
-  //       color: "#333"
-  //     }
-  //   },
-  //   fill: {
-  //     type: 'gradient',
-  //     gradient: {
-  //       gradientToColors: ['#F55555', '#6078ea', '#6094ea']
-  //     },
-  //   },
-  //   yaxis: {
-  //     decimalsInFloat: 2,
-  //     opposite: true,
-  //     labels: {
-  //       offsetX: -10
-  //     }
-  //   }
-  // };
 
   // function getDadosMaquina(idAgencia) {
   //   fetch("/dashAgencias/getDadosMaquina").then(function (resposta) {
@@ -199,9 +405,9 @@ function graficoAgencia(idAgencia, maquinas) {
   //   });
   // }
 
-  // function getDadosMaquina(yrange){
-  //   return Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
-  // }
+  function getDadosMaquina(yrange){
+    return Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+  }
 
   // /*function atualizarDados(){
   //   var cpuData = dados.cpu;
@@ -225,222 +431,40 @@ function graficoAgencia(idAgencia, maquinas) {
 
   // }*/
 
-  // var optionsProgress1 = {
-  //   chart: {
-  //     height: 70,
-  //     type: 'bar',
-  //     stacked: true,
-  //     sparkline: {
-  //       enabled: true
-  //     }
-  //   },
-  //   plotOptions: {
-  //     bar: {
-  //       horizontal: true,
-  //       barHeight: '20%',
-  //       colors: {
-  //         backgroundBarColors: ['#40475D']
-  //       }
-  //     },
-  //   },
-  //   stroke: {
-  //     width: 0,
-  //   },
-  //   series: [{
-  //     name: 'CPU',
-  //     data: [44]
-  //   }],
-  //   title: {
-  //     floating: true,
-  //     offsetX: -10,
-  //     offsetY: 5,
-  //     text: 'CPU'
-  //   },
-  //   subtitle: {
-  //     floating: true,
-  //     align: 'right',
-  //     offsetY: 0,
-  //     text: '44%',
-  //     style: {
-  //       fontSize: '20px'
-  //     }
-  //   },
-  //   tooltip: {
-  //     enabled: false
-  //   },
-  //   xaxis: {
-  //     categories: ['CPU'],
-  //   },
-  //   yaxis: {
-  //     max: 100
-  //   },
-  //   fill: {
-  //     opacity: 1
-  //   }
-  // }
+  window.setInterval(function () {
 
-  // var chartProgress1 = new ApexCharts(document.querySelector('#progress1'), optionsProgress1);
-  // chartProgress1.render();
+    //mexer aqui para passar as variaveis do banco com parametro
 
+    var p1Data = getDadosMaquina({ min: 10, max: 100 });
+    chartProgress1.updateOptions({
+      series: [{
+        data: [p1Data]
+      }],
+      subtitle: {
+        text: p1Data + "%"
+      }
+    })
 
-  // var optionsProgress2 = {
-  //   chart: {
-  //     height: 70,
-  //     type: 'bar',
-  //     stacked: true,
-  //     sparkline: {
-  //       enabled: true
-  //     }
-  //   },
-  //   plotOptions: {
-  //     bar: {
-  //       horizontal: true,
-  //       barHeight: '20%',
-  //       colors: {
-  //         backgroundBarColors: ['#40475D']
-  //       }
-  //     },
-  //   },
-  //   colors: ['#17ead9'],
-  //   stroke: {
-  //     width: 0,
-  //   },
-  //   series: [{
-  //     name: 'Memória',
-  //     data: [80]
-  //   }],
-  //   title: {
-  //     floating: true,
-  //     offsetX: -10,
-  //     offsetY: 5,
-  //     text: 'Memória'
-  //   },
-  //   subtitle: {
-  //     floating: true,
-  //     align: 'right',
-  //     offsetY: 0,
-  //     text: '80%',
-  //     style: {
-  //       fontSize: '20px'
-  //     }
-  //   },
-  //   tooltip: {
-  //     enabled: false
-  //   },
-  //   xaxis: {
-  //     categories: ['Memória'],
-  //   },
-  //   yaxis: {
-  //     max: 100
-  //   },
-  //   fill: {
-  //     type: 'gradient',
-  //     gradient: {
-  //       inverseColors: false,
-  //       gradientToColors: ['#6078ea']
-  //     }
-  //   },
-  // }
+    var p2Data = getDadosMaquina({ min: 10, max: 100 });
+    chartProgress2.updateOptions({
+      series: [{
+        data: [p2Data]
+      }],
+      subtitle: {
+        text: p2Data + "%"
+      }
+    })
 
-  // var chartProgress2 = new ApexCharts(document.querySelector('#progress2'), optionsProgress2);
-  // chartProgress2.render();
-
-
-  // var optionsProgress3 = {
-  //   chart: {
-  //     height: 70,
-  //     type: 'bar',
-  //     stacked: true,
-  //     sparkline: {
-  //       enabled: true
-  //     }
-  //   },
-  //   plotOptions: {
-  //     bar: {
-  //       horizontal: true,
-  //       barHeight: '20%',
-  //       colors: {
-  //         backgroundBarColors: ['#40475D']
-  //       }
-  //     },
-  //   },
-  //   colors: ['#f02fc2'],
-  //   stroke: {
-  //     width: 0,
-  //   },
-  //   series: [{
-  //     name: 'Disco',
-  //     data: [74]
-  //   }],
-  //   fill: {
-  //     type: 'gradient',
-  //     gradient: {
-  //       gradientToColors: ['#6094ea']
-  //     }
-  //   },
-  //   title: {
-  //     floating: true,
-  //     offsetX: -10,
-  //     offsetY: 5,
-  //     text: 'Disco'
-  //   },
-  //   subtitle: {
-  //     floating: true,
-  //     align: 'right',
-  //     offsetY: 0,
-  //     text: '74%',
-  //     style: {
-  //       fontSize: '20px'
-  //     }
-  //   },
-  //   tooltip: {
-  //     enabled: false
-  //   },
-  //   xaxis: {
-  //     categories: ['Disco'],
-  //   },
-  //   yaxis: {
-  //     max: 100
-  //   },
-  // }
-
-  // var chartProgress3 = new ApexCharts(document.querySelector('#progress3'), optionsProgress3);
-  // chartProgress3.render();
-
-  // window.setInterval(function () {
-
-  //   //mexer aqui para passar as variaveis do banco com parametro
-
-  //   var p1Data = getDadosMaquina({ min: 10, max: 100 });
-  //   chartProgress1.updateOptions({
-  //     series: [{
-  //       data: [p1Data]
-  //     }],
-  //     subtitle: {
-  //       text: p1Data + "%"
-  //     }
-  //   })
-
-  //   var p2Data = getDadosMaquina({ min: 10, max: 100 });
-  //   chartProgress2.updateOptions({
-  //     series: [{
-  //       data: [p2Data]
-  //     }],
-  //     subtitle: {
-  //       text: p2Data + "%"
-  //     }
-  //   })
-
-  //   var p3Data = getDadosMaquina({ min: 10, max: 100 });
-  //   chartProgress3.updateOptions({
-  //     series: [{
-  //       data: [p3Data]
-  //     }],
-  //     subtitle: {
-  //       text: p3Data + "%"
-  //     }
-  //   })
+    var p3Data = getDadosMaquina({ min: 10, max: 100 });
+    chartProgress3.updateOptions({
+      series: [{
+        data: [p3Data]
+      }],
+      subtitle: {
+        text: p3Data + "%"
+      }
+    })
 
 
 
-  // }, 3000);
+  }, 3000);
