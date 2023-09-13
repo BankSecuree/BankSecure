dbd191bfeacce13a6d892c0aebf96588aeffcee6
brunolimabh <bruno.lima@sptech.school>
2023-09-13 19:17:20 -0300

    refactor: Organizando código de teste para agencia Rudge Ramos

--- API/simulacaoItau.py
+++ API/simulacaoItau.py
@@ -5,77 +5,74 @@ import mysql.connector
 import requests 
 import json
 import platform
+import time
  
 conexao = mysql.connector.connect(user='bs_itau', password='Itau_100', host='localhost', database='bankSecure')
 
 cursor = conexao.cursor()
 
-def pegar_dados():
-  
-    exibiu = False
+def rudge_ramos():
     cont = 0
-
-    while cont <= 1:
     
-     data = datetime.now()
-     data = data.strftime('%Y/%m/%d %H:%M:%S')
-
-     user = [user[0] for user in psutil.users()]
-     user = user[0]
-     qtd_core = psutil.cpu_count(logical=False)
-
-     cpu_porcent = psutil.cpu_percent(interval=1)
-     cpu_speed = psutil.cpu_freq().current / pow(10,3)
-     cpu_speed_max = psutil.cpu_freq().max / pow(10,3)
-
-     ram_total = (psutil.virtual_memory().total) / pow(10,9)
-     ram_used = (psutil.virtual_memory().used) / pow(10,9)
-     ram_percent = psutil.virtual_memory().percent
-
-     so = platform.system()
-
-     if (so == 'Windows'):
-         disc_total = psutil.disk_usage('C:\\').total / pow(10,9)
-         disc_used = psutil.disk_usage('C:\\').used / pow(10,9)
-         disc_percent = psutil.disk_usage('C:\\').percent
-     elif (so == 'Linux'):
-         disc_total = psutil.disk_usage('/bin').total / pow(10,9)
-         disc_used = psutil.disk_usage('/bin').used / pow(10,9)
-         disc_percent = psutil.disk_usage('/bin').percent
-   
-    # Alerta Slack
-     if (exibiu == False):
-         if (ram_percent > 83):
-             mensagem = {"text": f"""
-            🚨ALERTA🚨
-
-            Protocolo  => 837021
-            Data          => {data}
-            User          => {user}
-            Descrição  => {"Sua memória RAM ultrapassou:"} {ram_percent}%  
-            """}
-             chatItau = "https://hooks.slack.com/services/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"
-
-             postMsg = requests.post(chatItau, data=json.dumps(mensagem))
-             exibiu = True
-
-     comp1 = "Memória"
-     comp2 = "CPU"
-     comp3 = "Disco"
-     if (cont == 0):
-         cursor.execute(f"CALL inserirDadosMaquina ('SI-1', '{comp1}', {ram_percent:.2f}, '{comp2}', {cpu_porcent}, '{comp3}', {disc_percent}, NOW());")
+    qtd_core = psutil.cpu_count(logical=False)
+    cpu_um_speed_max = psutil.cpu_freq().max / pow(10,3)
+    ram_um_total = (psutil.virtual_memory().total) / pow(10,9)
+    so = platform.system()
+    if (so == 'Windows'):
+        disco_um_total = psutil.disk_usage('C:\\').total / pow(10,9)
+    elif (so == 'Linux'):
+        disco_um_total = psutil.disk_usage('/bin').total / pow(10,9)
     
-         cursor.execute(f"CALL inserirDadosMaquina ('MI-1', '{comp1}', {ram_percent:.2f}, '{comp2}', {cpu_porcent}, '{comp3}', {disc_percent}, NOW());")
-     
-         cursor.execute(f"CALL inserirDadosMaquina ('MI-2', '{comp1}', {ram_percent:.2f}, '{comp2}', {cpu_porcent}, '{comp3}', {disc_percent}, NOW());")
-     
-         cursor.execute(f"CALL inserirDadosMaquina ('MI-3', '{comp1}', {ram_percent:.2f}, '{comp2}', {cpu_porcent}, '{comp3}', {disc_percent}, NOW());")
+    while True:
+        data = datetime.now()
+        data = data.strftime('%Y/%m/%d %H:%M:%S')
+        cpu_m1 = psutil.cpu_percent(interval=1)
+        cpu_m1_speed = psutil.cpu_freq().current / pow(10,3)
+        ram_m1_used = (psutil.virtual_memory().used) / pow(10,9)
+        ram_m1 = psutil.virtual_memory().percent
+        if (so == 'Windows'):
+            disco_m1 = psutil.disk_usage('C:\\').percent
+        elif (so == 'Linux'):
+            disco_m1 = psutil.disk_usage('/bin').percent
+        
+        cpu_s1 = cpu_m1 * 1.15
+        cpu_s1 = 100 if cpu_s1 >= 100 else cpu_s1
+        ram_s1 = ram_m1
+        ram_s1 = 100 if ram_s1 >= 100 else ram_s1
+        disco_s1 = disco_m1 * 1.2
+        disco_s1 = 100 if disco_s1 >= 100 else disco_s1
 
-     conexao.commit()
-     cont+=1
+        ram_m1 = ram_m1 * 0.90
+        ram_m1 = 0 if ram_m1 <= 0 else ram_m1
+        
+        cpu_m2 = cpu_m1 * 1.05
+        cpu_m2 = 100 if cpu_m2 >= 100 else cpu_m2
+        ram_m2 = ram_m1 * 0.875
+        ram_m2 = 0 if ram_m2 <= 0 else ram_m2
+        disco_m2 = disco_m1 * 1.05
+        disco_m2 = 100 if disco_m2 >= 100 else disco_m2
+
+        cpu_m3 = cpu_m1 * 0.95
+        cpu_m3 = 0 if cpu_m3 <= 0 else cpu_m3
+        ram_m3 = ram_m1 * 0.85
+        cpu_m3 = 0 if cpu_m3 <= 0 else cpu_m3
+        disco_m3 = disco_m1 * 0.95
+        disco_m3 = 0 if disco_m3 <= 0 else disco_m3
+
+        cursor.execute(f"CALL inserirDadosMaquina ('MI-1', 'Memória', {ram_m1:.1f}, 'CPU', {cpu_m1:.1f}, 'Disco', {disco_m1:.1f}, NOW());")
+        cursor.execute(f"CALL inserirDadosMaquina ('MI-2', 'Memória', {ram_m2:.1f}, 'CPU', {cpu_m2:.1f}, 'Disco', {disco_m2:.1f}, NOW());")
+        cursor.execute(f"CALL inserirDadosMaquina ('MI-3', 'Memória', {ram_m3:.1f}, 'CPU', {cpu_m3:.1f}, 'Disco', {disco_m3:.1f}, NOW());")
+        cursor.execute(f"CALL inserirDadosMaquina ('SI-1', 'Memória', {ram_s1:.1f}, 'CPU', {cpu_s1:.1f}, 'Disco', {disco_s1:.1f}, NOW());")
+            
+        conexao.commit()
+        cont+=1
+
+        # time.sleep(1)
+        rudge_ramos()
  
+
 if (conexao.is_connected()):
     print("A Conexão ao MySql foi iniciada ")
-    pegar_dados()
+    rudge_ramos()
 else:
     print("Houve erro ao conectar")
