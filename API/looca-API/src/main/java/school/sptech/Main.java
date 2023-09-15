1f5564a56b911f707e7f4c6b78eb7034e2c3a746
KauanCursino <kauan.souza@sptech.school>
2023-09-15 09:20:27 -0300

    feat(looca API): Adição da API Looca ao projeto

---
+++ API/looca-API/src/main/java/school/sptech/Main.java
@@ -0,0 +1,12 @@
+package school.sptech;
+
+
+import com.github.britooo.looca.api.core.Looca;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Looca looca = new Looca();
+
+    }
+}
