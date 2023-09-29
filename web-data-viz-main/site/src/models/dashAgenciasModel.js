3afa54003db97328ba68d962b5c3e2ff260a4783
babi-barbato <barbara.oliveira@sptech.school>
2023-09-28 21:15:21 -0300

    dados atualizam sozinhos

--- web-data-viz-main/site/src/models/dashAgenciasModel.js
+++ web-data-viz-main/site/src/models/dashAgenciasModel.js
@@ -29,7 +29,18 @@ function graficoAgencia(idAgencia) {
     return database.executar(instrucao);
 }
 
+function dadosAnalista(agencia,componente) {
+    console.log("ACESSEI O dashAnalistaModel \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function exibirDonut()");
+
+    var instrucao = `
+    select * from registros where fkComponente = ${componente} and fkMaquina = ${agencia} order by dataHora desc limit 7;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
 module.exports = {
+    dadosAnalista,
     exibirListaAgencias,
     graficoAgencia,
     exibirView
