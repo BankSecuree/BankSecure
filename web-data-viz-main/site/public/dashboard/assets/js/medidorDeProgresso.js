10074f0c178d8e248d93ecc24a62a9617c3a4b3d
arthurRFmoraes <arthur.moraes@sptech.school>
2023-10-08 12:45:19 -0300

    tela gerente estatica finalizada

--- web-data-viz-main/site/public/dashboard/assets/js/medidorDeProgresso.js
+++ web-data-viz-main/site/public/dashboard/assets/js/medidorDeProgresso.js
@@ -1,9 +1,17 @@
-function alterarPeriodoVizualizacao(periodo){
-    if(periodo == 1){
-        
-    }else if(periodo == 2){
-
-    }else{
-
+function alterarPeriodoVizualizacao(campoParaAlterar, periodo){
+    let periodos = {'1': "Hoje",
+                    '2':"Este Mês",
+                    '3':"Este Ano"}
+    console.log(periodos[periodo])
+    console.log(periodos.p1)
+    let periodoId = document.getElementById(campoParaAlterar + "_period_info")
+    if(campoParaAlterar == 'cpu'){
+        periodoId.innerHTML = periodos[periodo]
+    }else if(campoParaAlterar == 'ram'){
+        periodoId.innerHTML = periodos[periodo]
+    }else if(campoParaAlterar == 'hd'){
+        periodoId.innerHTML = periodos[periodo]
+    }else if(campoParaAlterar == 'graficoDonut'){
+        periodoId.innerHTML = periodos[periodo]
     }
 }
