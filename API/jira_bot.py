d8e8167a9346927a2306d4939f158b2e08143134
arthurRFmoraes <arthur.moraes@sptech.school>
2023-10-24 08:52:27 -0300

    api python completa

--- API/jira_bot.py
+++ API/jira_bot.py
@@ -3,86 +3,101 @@ from requests.auth import HTTPBasicAuth
 import json
 from datetime import datetime
 import psutil
-repetir = True
+
+
 class issue_manipulation:
-  def __init__(self, maquina: str, componente: str, porcentagem_uso: float, prioridade: int):
-    self.maquina = maquina
-    self.componente = componente
-    self.porcentagem_uso = porcentagem_uso
-    self.prioridade = prioridade
-    self.url = 'https://banksecure.atlassian.net/rest/api/3/'
-    self.auth = HTTPBasicAuth('suporte.banksecure@gmail.com', 'ATATT3xFfGF0OVDj2XBJchVqK5oAlkA8Ecyntm--fJBiKaGWEV7flX75vaaon8he5Hk5f8qfHpFmZpDNLqmUWSRuAgxmXlA4m-huvJUK5hfkyolNFsL0Lt8WE1RQ8KtcWTYQPzvtmH-bnDXuE6Di-jf23tGGCxAMxNCc5OgThtqnoCycmg7z7b8=F851C43C')
-  def create_issue(self):
-    url = self.url + "issue"
-    mensagem_alerta = f'''Componente: {self.componente}\nPorcentagem de uso: {self.porcentagem_uso}'''
-    headers = {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
+    def __init__(
+        self, maquina: str, componente: str, porcentagem_uso: float, prioridade: int
+    ):
+        self.maquina = maquina
+        self.componente = componente
+        self.porcentagem_uso = porcentagem_uso
+        self.prioridade = str(prioridade)
+        self.url = "https://banksecure.atlassian.net/rest/api/3/"
+        self.auth = HTTPBasicAuth(
+            "suporte.banksecure@gmail.com",
+            "ATATT3xFfGF0npBFytU7RID76c8RWa5_r0f-k0fylHHwlPJdlpSSkl4TizynJLj0ADs-hA6MvYAU7qxaUXhj-R0wSa2HU-pD3DAN8DerGwckV69RsjkQep0pVZGK0ol3u6VBUorcL8ihiSLJAO6EGIvudv-lJsX8GRnaU1w0Zib5yK4HzhApwq4=35F55320",
+        )
+        self.__verify_issue()
 
-    payload = json.dumps({
-        'fields':{
-            'summary': f'Alerta máquina {self.maquina}',
-            'project':{'key':'BSITAU'},
-            'issuetype': {'name': 'Task'},
-            'priority': {'id': self.prioridade},
-            'description': {'content': [{'content': [
-                                          {
-                                            'text': mensagem_alerta,
-                                            'type': 'text'
-                                          }],
-                                        'type': 'paragraph'}],
-                                        'type': 'doc',
-                                        'version': 1}
-          }
-    })
-    response = requests.request(
-      'POST',
-      url,
-      data=payload,
-      headers=headers,
-      auth=self.auth
-    )
-    print(response.status_code)
+    def __post_issue(self):
+        url = self.url + "issue"
+        mensagem_alerta = f"""Componente: {self.componente}\nPorcentagem de uso: {self.porcentagem_uso}"""
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        payload = json.dumps(
+            {
+                "fields": {
+                    "summary": f"Alerta máquina {self.maquina}",
+                    "project": {"key": "BSITAU"},
+                    "issuetype": {"name": "Task"},
+                    "priority": {"id": self.prioridade},
+                    "description": {
+                        "content": [
+                            {
+                                "content": [{"text": mensagem_alerta, "type": "text"}],
+                                "type": "paragraph",
+                            }
+                        ],
+                        "type": "doc",
+                        "version": 1,
+                    },
+                }
+            }
+        )
 
-  def get_issue(self):
-    url = self.url + "search"
-    headers = {
-      "Accept": "application/json"
-      }
-    response = requests.request(
-    "GET",
-    url,
-    headers=headers,
-    auth=self.auth
-    )
-    for i in response.json()['issues']:
-      try:
-        # print(i["fields"]["description"]["content"][0]["content"][0]["text"])
-        
-        print(i["fields"]["status"]["name"])
+        response = requests.request(
+            "POST", url, data=payload, headers=headers, auth=self.auth
+        )
 
-        prioridade_alerta = int(i["fields"]["priority"]["id"])
-        status_alerta = i["fields"]["status"]["name"]
-        maquina_em_alerta = str.replace(i["fields"]["summary"], "Alerta máquina ", "")
+    def __update_issue(self, issueKey: str):
+        url = f"{self.url}issue/{issueKey}"
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
 
-        if(status_alerta != "Concluído" and prioridade_alerta > self.prioridade):
-          problema.create_issue()
+        mensagem_alerta = f"""Componente: {self.componente}\nPorcentagem de uso: {self.porcentagem_uso}"""
+        headers = {"Accept": "application/json", "Content-Type": "application/json"}
+        payload = json.dumps(
+            {
+                "fields": {
+                    "summary": f"Alerta máquina {self.maquina}",
+                    "project": {"key": "BSITAU"},
+                    "issuetype": {"name": "Task"},
+                    "priority": {"id": self.prioridade},
+                    "description": {
+                        "content": [
+                            {
+                                "content": [{"text": mensagem_alerta, "type": "text"}],
+                                "type": "paragraph",
+                            }
+                        ],
+                        "type": "doc",
+                        "version": 1,
+                    },
+                }
+            }
+        )
+        response = requests.request(
+            "PUT", url, data=payload, headers=headers, auth=self.auth
+        )
 
-        valor_alerta = str.replace(i["fields"]["description"]["content"][0]["content"][0]["text"], f"Componente: {self.componente}\nPorcentagem de uso: ", "")
-        print(maquina_em_alerta)
-        print(float(valor_alerta))
-        print("\n")
-      except:
-        pass
-        # print(i["key"])
-        # print(i["fields"]["description"])
-        # print("\n")
-    # print(response.json()['issues'])
-    # print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-
-
-problema = issue_manipulation('MI-1','CPU',19.8, 1)
-problema.get_issue()
-problema.create_issue()
-# print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(',', ': ')))
+    def __verify_issue(self):
+        url = self.url + "search"
+        headers = {"Accept": "application/json"}
+        response = requests.request("GET", url, headers=headers, auth=self.auth)
+        chamados = []
+        for i in response.json()["issues"]:
+            status_alerta = i["fields"]["status"]["name"]
+            maquina_em_alerta = str.replace(
+                i["fields"]["summary"], "Alerta máquina ", ""
+            )
+            if status_alerta != "Concluído" and maquina_em_alerta == self.maquina:
+                chamados.append(i)
+        if len(chamados) == 0:
+            self.__post_issue()
+        else:
+            for i in chamados:
+                id = i["id"]
+                prioridade_alerta = int(i["fields"]["priority"]["id"])
+                if prioridade_alerta > int(self.prioridade):
+                    self.__update_issue(id)
+                    status_alerta = i["fields"]["status"]["name"]
+                    break
--- API/monitor_hardware.py
+++ API/monitor_hardware.py
@@ -1,16 +1,22 @@
 import psutil
-import time 
+import time
 from tkinter import *
 from tkinter import ttk
 from datetime import datetime
 import os
 import mysql.connector
-import requests 
+import requests
 import json
 import platform
 from jira_bot import issue_manipulation
- 
-conexao = mysql.connector.connect(user='user_bankSecure', password='Urubu_100', host='localhost', database='bankSecure', auth_plugin = "mysql_native_password")
+
+conexao = mysql.connector.connect(
+    user="user_bankSecure",
+    password="Urubu_100",
+    host="localhost",
+    database="bankSecure",
+    auth_plugin="mysql_native_password",
+)
 cursor = conexao.cursor()
 
 maquina = ""
@@ -18,83 +24,121 @@ maquina = ""
 prioridades = {"maior": 1, "alta": 2, "media": 3, "pequena": 4, "menor": 1}
 
 
-if (conexao.is_connected()):
-        print("A Conexão ao MySql foi iniciada ")
+if conexao.is_connected():
+    print("A Conexão ao MySql foi iniciada ")
 else:
-     print("Houve erro ao conectar")
+    print("Houve erro ao conectar")
+
 
 def login():
-     email = email_entry.get()
-     senha = senha_entry.get()
-     cursor.execute(f'SELECT * FROM usuario WHERE email = "{email}" AND senha = "{senha}"')
-     result = cursor.fetchall();
-     if(len(result) > 0):
-         login_frame.grid_forget()
-         main_frame.grid(column=0, row=0)
-         pegar_dados()
-         
-     else:
+    email = email_entry.get()
+    senha = senha_entry.get()
+    cursor.execute(
+        f'SELECT * FROM usuario WHERE email = "{email}" AND senha = "{senha}"'
+    )
+    result = cursor.fetchall()
+    if len(result) > 0:
+        login_frame.grid_forget()
+        main_frame.grid(column=0, row=0)
+        pegar_dados()
+
+    else:
         mensagem_label.config(text="Credenciais incorretas")
 
 
 # login()
 def pegar_dados():
-  
+    comp1 = "Memória"
+    comp2 = "CPU"
+    comp3 = "Disco"
+    maquina = "MI-1"
+
     while True:
-    
-     data = datetime.now()
-     data = data.strftime('%Y/%m/%d %H:%M:%S')
+        data = datetime.now()
+        data = data.strftime("%Y/%m/%d %H:%M:%S")
 
-    #  user = [user[0] for user in psutil.users()]
-    #  user = user[0]
-     user = "MI-1"
-     qtd_core = psutil.cpu_count(logical=False)
+        qtd_core = psutil.cpu_count(logical=False)
 
-     cpu_porcent = psutil.cpu_percent(interval=1)
-     cpu_speed = psutil.cpu_freq().current / pow(10,3)
-     cpu_speed_max = psutil.cpu_freq().max / pow(10,3)
+        cpu_percent = psutil.cpu_percent(interval=1)
+        cpu_speed = psutil.cpu_freq().current / pow(10, 3)
+        cpu_speed_max = psutil.cpu_freq().max / pow(10, 3)
 
-     so = platform.system()
-     processor = platform.processor()
+        so = platform.system()
+        processor = platform.processor()
 
-     if (so == 'Windows'):
-    # DIRETÓRIO PARA WINDOWS
+        if so == "Windows":
+            # DIRETÓRIO PARA WINDOWS
 
-         disc_total = psutil.disk_usage('C:\\').total / pow(10,9)
-         disc_used = psutil.disk_usage('C:\\').used / pow(10,9)
-         disc_percent = psutil.disk_usage('C:\\').percent
-     elif (so == 'Linux'):
-    # DIRETÓRIO PARA LINUX
-         disc_total = psutil.disk_usage('/bin').total / pow(10,9)
-         disc_used = psutil.disk_usage('/bin').used / pow(10,9)
-         disc_percent = psutil.disk_usage('/bin').percent
+            disc_total = psutil.disk_usage("C:\\").total / pow(10, 9)
+            disc_used = psutil.disk_usage("C:\\").used / pow(10, 9)
+            disc_percent = psutil.disk_usage("C:\\").percent
+        elif so == "Linux":
+            # DIRETÓRIO PARA LINUX
+            disc_total = psutil.disk_usage("/bin").total / pow(10, 9)
+            disc_used = psutil.disk_usage("/bin").used / pow(10, 9)
+            disc_percent = psutil.disk_usage("/bin").percent
 
-     ram_total = (psutil.virtual_memory().total) / pow(10,9)
-     ram_used = (psutil.virtual_memory().used) / pow(10,9)
-     ram_percent = psutil.virtual_memory().percent
+        ram_total = (psutil.virtual_memory().total) / pow(10, 9)
+        ram_used = (psutil.virtual_memory().used) / pow(10, 9)
+        ram_percent = psutil.virtual_memory().percent
 
-     comp1 = "Memória"
-     comp2 = "CPU"
-     comp3 = "Disco"
-     cursor.execute(f"CALL inserirDadosMaquina ('{user}', '{comp1}', {ram_percent:.2f}, '{comp2}', {cpu_porcent}, '{comp3}', {disc_percent}, NOW());")
-     data_label.config(text=f'{data}')
-     cpu_porcentagem_uso_label.config(text=f'Porcentagem de uso\n{cpu_porcent:.2f}%')
-     cpu_velocidade_label.config(text=f'Velocidade\n{cpu_speed:.2f}GHz')
-     cpu_velocidade_maxima_label.config(text=f'Velocidade Maxima\n{cpu_speed_max:.2f}GHz')
-     disco_porcentagem_uso_label.config(text=f'"Porcentagem de uso\n{disc_percent:.2f}%')
-     disco_total_label.config(text=f'Total\n{disc_total:.2f}GB')
-     disco_uso_label.config(text=f'Em uso\n{disc_used:.2f}GB')
-     ram_porcentagem_uso_label.config(text=f'"Porcentagem de uso\n{ram_percent:.2f}%')
-     ram_total_label.config(text=f'Total\n{ram_total:.2f}GB')
-     ram_uso_label.config(text=f'Em uso\n{ram_used:.2f}GB')
+        cpu_percent = round(cpu_percent, 2)
+        cpu_speed = round(cpu_speed, 2)
+        cpu_speed_max = round(cpu_speed_max, 2)
 
-    #Gravar os dados na tabela definitiva
-     conexao.commit()
-     # texto_cotacao['text'] = msgOpen
-     root.update()
-     time.sleep(1)
+        disc_total = round(disc_total, 2)
+        disc_used = round(disc_used, 2)
+        disc_percent = round(disc_percent, 2)
 
-     
+        ram_total = round(ram_total, 2)
+        ram_used = round(ram_used, 2)
+        ram_percent = round(ram_percent, 2)
+
+        if ram_percent >= 60.0 and ram_percent < 75.0:
+            problema = issue_manipulation(maquina, comp1, ram_percent, 3)
+        elif ram_percent >= 75.0 and ram_percent < 90:
+            problema = issue_manipulation(maquina, comp1, ram_percent, 2)
+        elif ram_percent >= 90:
+            problema = issue_manipulation(maquina, comp1, ram_percent, 1)
+
+        if cpu_percent >= 60.0 and cpu_percent < 75.0:
+            problema = issue_manipulation(maquina, comp2, cpu_percent, 3)
+        elif cpu_percent >= 75.0 and cpu_percent < 90:
+            problema = issue_manipulation(maquina, comp2, cpu_percent, 2)
+        elif cpu_percent >= 90:
+            problema = issue_manipulation(maquina, comp2, cpu_percent, 1)
+
+        if disc_percent >= 60.0 and disc_percent < 75.0:
+            problema = issue_manipulation(maquina, comp3, disc_percent, 3)
+        elif disc_percent >= 75.0 and disc_percent < 90:
+            problema = issue_manipulation(maquina, comp3, disc_percent, 2)
+        elif disc_percent >= 90:
+            print("esse foi acessado")
+            problema = issue_manipulation(maquina, comp3, disc_percent, 1)
+
+        cursor.execute(
+            f"CALL inserirDadosMaquina ('{maquina}', '{comp1}', {ram_percent}, '{comp2}', {cpu_percent}, '{comp3}', {disc_percent}, NOW());"
+        )
+        data_label.config(text=f"{data}")
+        cpu_percentagem_uso_label.config(text=f"Porcentagem de uso\n{cpu_percent}%")
+        cpu_velocidade_label.config(text=f"Velocidade\n{cpu_speed}GHz")
+        cpu_velocidade_maxima_label.config(
+            text=f"Velocidade Maxima\n{cpu_speed_max}GHz"
+        )
+        disco_porcentagem_uso_label.config(text=f'"Porcentagem de uso\n{disc_percent}%')
+        disco_total_label.config(text=f"Total\n{disc_total}GB")
+        disco_uso_label.config(text=f"Em uso\n{disc_used}GB")
+        ram_porcentagem_uso_label.config(text=f'"Porcentagem de uso\n{ram_percent}%')
+        ram_total_label.config(text=f"Total\n{ram_total}GB")
+        ram_uso_label.config(text=f"Em uso\n{ram_used}GB")
+
+        # Gravar os dados na tabela definitiva
+        conexao.commit()
+        # texto_cotacao['text'] = msgOpen
+        root.update()
+        time.sleep(1)
+
+
 # cria a root
 root = Tk()
 style = ttk.Style()
@@ -119,7 +163,7 @@ email_entry.grid(column=2, row=2)
 senha_label = ttk.Label(login_frame, text="Senha: ")
 senha_label.grid(column=1, row=3, padx=10, pady=10)
 
-senha_entry = ttk.Entry(login_frame, width=25)
+senha_entry = ttk.Entry(login_frame, width=25, show="*")
 senha_entry.grid(column=2, row=3)
 
 mensagem_label = ttk.Label(login_frame, text="")
@@ -137,7 +181,7 @@ usuario_label = ttk.Label(main_frame, text="Usuario: xxx")
 maquina_label = ttk.Label(main_frame, text="Maquina: xxx")
 
 cpu_header_label = ttk.Label(main_frame, text="CPU")
-cpu_porcentagem_uso_label = ttk.Label(main_frame, text="Porcentagem de uso\n")
+cpu_percentagem_uso_label = ttk.Label(main_frame, text="Porcentagem de uso\n")
 cpu_velocidade_label = ttk.Label(main_frame, text="Velocidade\n")
 cpu_velocidade_maxima_label = ttk.Label(main_frame, text="Velocidade Maxima\n")
 
@@ -157,7 +201,7 @@ usuario_label.grid(column=8, row=2, columnspan=2)
 maquina_label.grid(column=8, row=3, columnspan=2)
 
 cpu_header_label.grid(column=2, row=5, padx=5, pady=5)
-cpu_porcentagem_uso_label.grid(column=3, row=5, columnspan=2, padx=5, pady=5)
+cpu_percentagem_uso_label.grid(column=3, row=5, columnspan=2, padx=5, pady=5)
 cpu_velocidade_label.grid(column=5, row=5, padx=5, pady=5)
 cpu_velocidade_maxima_label.grid(column=7, row=5, columnspan=2, padx=5, pady=5)
 
@@ -174,5 +218,3 @@ ram_uso_label.grid(column=7, row=7, padx=5, pady=5)
 
 
 root.mainloop()
-
-
